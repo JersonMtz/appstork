f21e84e93dff0a0e7fa8c14c6c451a52cfc623ed
JersonMtz <gxnz_17@live.com.mx>
2020-08-26 22:39:16 -0600

    HEROKU READY

--- server.js
+++ server.js
@@ -1,28 +1,14 @@
-/*const express = require('express');
-const path = require('path');
-
-const app = express();
-
-app.use(express.static('./dist/stork'));
-
-app.get('/*', function(req,res){
-    res.sendFile('index.html', { root: 'dist/stork/'});
-});
-
-app.listen(process.env.PORT || 4200, () => {
-    console.log('servidor ON en puerto 4200...');
-});*/
-
 const express = require('express');
+
 const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/stork'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/stork/index.html'));
+app.use(express.static(__dirname + '/dist/stork'));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname +
+        '/dist/stork/index.html'));
 });
 
-app.listen(process.env.PORT || 4200, () => {
-    console.log('servidor ON en puerto 4200...');
-});
+app.listen(process.env.PORT || 8080);
